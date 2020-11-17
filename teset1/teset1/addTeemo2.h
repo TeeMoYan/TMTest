3155cfc38710ee54b840cec2c723a719c05afde2
yanzhen <yanzhen@iboxpay.com>
2020-11-17 10:55:24 +0800

    add teemo2

---
+++ teset1/teset1/addTeemo2.h
@@ -0,0 +1,24 @@
+//
+//  addTeemo2.h
+//  teset1
+//
+//  Created by 闫振 on 2020/11/17.
+//
+//  Copyright (C) 2011-2020 ShenZhen iBOXCHAIN Information Technology Co.,Ltd. 
+//                             All rights reserved.
+//
+//      This  software  is  the  confidential  and  proprietary  information  of
+//  iBOXCHAIN  Company  of  China. ("Confidential Information"). You  shall  not
+//  disclose such Confidential Information and shall use it only in accordance
+//  with the terms of the contract agreement you entered into with iBOXCHAIN inc.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface addTeemo2 : UIView
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ teset1/teset1/addTeemo2.m
@@ -0,0 +1,28 @@
+//
+//  addTeemo2.m
+//  teset1
+//
+//  Created by 闫振 on 2020/11/17.
+//
+//  Copyright (C) 2011-2020 ShenZhen iBOXCHAIN Information Technology Co.,Ltd. 
+//                             All rights reserved.
+//
+//      This  software  is  the  confidential  and  proprietary  information  of
+//  iBOXCHAIN  Company  of  China. ("Confidential Information"). You  shall  not
+//  disclose such Confidential Information and shall use it only in accordance
+//  with the terms of the contract agreement you entered into with iBOXCHAIN inc.
+//
+
+#import "addTeemo2.h"
+
+@implementation addTeemo2
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
